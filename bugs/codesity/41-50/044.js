44ee4524aa189155e1b577afd7e1be62f31c88f5
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-07-04 21:35:38 +0200

    Add 044.js

---
+++ bugs/codesity/41-50/044.js
@@ -0,0 +1,10 @@
+const dictionary = new Map()
+// dictionary.car = "samochód"
+// dictionary.dog = "pies"
+dictionary.set('car', 'samochód')
+dictionary.set('dog', 'pies')
+
+console.log(dictionary.get("car"))
+console.log(dictionary.has("dog"))
+
+// https://codisity.pl/100-bugow-js/d05d69bf-78ae-4c42-a213-81006fa7e6cb
