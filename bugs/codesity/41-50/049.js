32d4cbdcdc7dfc0eff031479075e0edc44840424
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-07-04 22:49:47 +0200

    add 049.js

---
+++ bugs/codesity/41-50/049.js
@@ -0,0 +1,5 @@
+const numbers = [43, 12, 1, 5, 26]
+const compareNumbers = (a, b) => a - b
+
+console.log(numbers.sort(compareNumbers));
+// https://codisity.pl/100-bugow-js/13faa6fb-1dfe-41df-9f96-62618e9ca0c4
